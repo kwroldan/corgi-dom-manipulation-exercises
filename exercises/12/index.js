cd89b4c2ea7b5eb34fb6bf5887a4eb3851bfd282
Kristofer <kroldan@ford.com>
2022-03-31 17:18:55 -0400

    Add exercise #12

---
+++ exercises/12/index.js
@@ -0,0 +1,6 @@
+const queens = document.querySelector(".queens");
+const ul = document.createElement("ul");
+const li = document.createElement("li");
+li.textContent = "Kiki";
+ul.append(li);
+queens.append(ul);
